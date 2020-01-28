afcff7de241d70ce061e5e1a612d70173d47b46c
Hughes <HUGHESZT1@GCC.EDU>
2020-01-28 14:29:10 -0500

    Linked submit button to form validity, started auth service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {Router} from '@angular/router'
+
+import {AuthService} from '../../services/auth.service'
 
 @Component({
   selector: 'app-login',
@@ -9,10 +12,44 @@ import {FormControl, Validators} from '@angular/forms';
 export class LoginComponent implements OnInit {
   emailControl = new FormControl('', [Validators.required, Validators.email]);
   passControl = new FormControl('',Validators.required)
+  CIDControl = new FormControl('', Validators.required)
+  loginForm: FormGroup
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({email: this.emailControl, password: this.passControl, CID: this.CIDControl})
+
+    //If we are already logged in, redirect to homescreen
+    this.authService.getAuth().subscribe(auth => {
+      if(auth){
+        this.router.navigate(['/']);
+      }
+    })
+  }
+
+  getEmailErrors(){
+    return this.emailControl.hasError('required') ? "Email is required" :
+        this.emailControl.hasError('email') ? "Email entered is not valid" :
+        "Email entered could not be read"
+  }
+
+  getPassErrors(){
+    return this.passControl.hasError('required') ? "Password is required" :
+        "Password is incorrect"
+  }
+
+  getCIDErrors(){
+    return this.CIDControl.hasError('required') ? "Company ID is required" :
+        "Company ID is incorrect"
+  }
+
+  onSubmit(){
+    console.log(this.loginForm.value)
   }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  login(email: string, password: string){
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.signInWithEmailAndPassword(email,password)
+      .then(userData => resolve(userData),
+      err => reject(err));
+    })
+  }
+
+  logout(){
+    this.afAuth.auth.signOut();
+  }
+
+  getAuth(){
+    return this.afAuth.authState;
+  }
+}
