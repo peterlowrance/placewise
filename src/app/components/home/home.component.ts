65174711ee388c42c331c35432f52a722ad74085
Peter <peter.lowrance@gmail.com>
2020-01-30 23:35:22 -0500

    Search: category/location and header styling

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,8 @@ import {SearchInterfaceService} from '../../services/search-interface.service';
 import {Item} from '../../models/Item';
 import {Router} from '@angular/router';
 import {HierarchyItem} from '../../models/HierarchyItem';
+import {Category} from '../../models/Category';
+import {Location} from '../../models/Location';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +14,8 @@ import {HierarchyItem} from '../../models/HierarchyItem';
 export class HomeComponent implements OnInit {
 
   selectedSearch = 'Categories';
+  categories: Category[];
+  locations: Location[];
   hierarchyItems: HierarchyItem[];
   items: Item[];
   columns: number;
@@ -23,10 +27,24 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     // Dummy data
     this.searchService.search('testSearch').subscribe(item => this.items = item);
-    /*for (let i = 0; i < 10; i++) {
-      this.hierarchyItems.push('hierarchy ' + i);
-      this.items.push('item' + i);
-    }*/
+    const c: Category = {
+      ID: 123,
+      name: 'tool',
+      parent: null,
+      imageUrl: 'https://previews.123rf.com/images/davids47/davids471403/davids47140300419/26802642-abstract-construction-tool-on-a-white-background.jpg',
+      children: []
+    };
+    const l: Location = {
+      ID: 122,
+      name: 'cabinet',
+      parent: null,
+      imageUrl: 'https://www.ikea.com/us/en/images/products/havsta-cabinet-with-plinth__0720107_PE732421_S5.JPG?f=s,',
+      children: []
+    };
+    this.categories = [c, c];
+    this.locations = [l];
+    this.hierarchyItems = [c, c, l, l, l, l, l];
+
     this.columns = (window.innerWidth <= this.breakpoint) ? 3 : 6;
   }
 
@@ -36,5 +54,9 @@ export class HomeComponent implements OnInit {
 
   goToItem(event, item) {
     this.router.navigate(['/item/', item.ID]);
+  }
+
+  toggleHierarchy(event) {
+    this.hierarchyItems = event.value === 'Categories' ? this.categories : this.locations;
   }
 }
