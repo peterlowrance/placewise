833eff4a4ef983fe4e302c9a6c783b6dad51f924
Ethan Weaver <terffinatorz@gmail.com>
2020-02-07 09:20:44 -0500

    service updated to hierarcyitem

--- src/app/services/search-interface.service.ts
+++ src/app/services/search-interface.service.ts
@@ -12,7 +12,7 @@ export abstract class SearchInterfaceService {
   abstract search(term: string) : Observable<Item[]>
   abstract getItem(ID: string): Observable<Item>
   abstract categoryItemsSearch(categoryID: string) : Observable<Item[]>
-  abstract categoryChildrenSearch(categoryID: string) : Observable<Category[]>
+  abstract categoryChildrenSearch(categoryID: string) : Observable<HierarchyItem[]>
   abstract locationItemsSearch(locationID: string) : Observable<Item[]>
   abstract locationChildrenSearch(locationID: string) : Observable<HierarchyItem[]>
 }
