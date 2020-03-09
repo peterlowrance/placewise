9d2e4e8ce019265d059f3a11168b39a9f916dbbf
peterlowrance <peter.lowrance@gmail.com>
2020-03-09 09:35:33 -0400

    Formatted and fixed login navigation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {MatSnackBar} from '@angular/material/snack-bar';
@@ -14,7 +14,7 @@ import {AuthService} from '../../services/auth.service';
 })
 export class LoginComponent implements OnInit {
   emailControl = new FormControl('', [Validators.required, Validators.email]);
-  passControl = new FormControl('',Validators.required);
+  passControl = new FormControl('', Validators.required);
   CIDControl = new FormControl('', Validators.required);
   loginForm: FormGroup;
 
@@ -24,55 +24,60 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private snack: MatSnackBar,
     private diag: MatDialog
-    ) { }
+  ) {
+  }
 
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({email: this.emailControl, password: this.passControl, CID: this.CIDControl})
+    this.loginForm = this.formBuilder.group({
+      email: this.emailControl,
+      password: this.passControl,
+      CID: this.CIDControl
+    });
 
     //If we are already logged in, redirect to homescreen
     this.authService.getAuth().subscribe(auth => {
-      if(auth){
+      if (auth) {
         //TODO: this is for testing purposes, replace below line with commented out code upon release to redirect login to main page
         //this.authService.logout()
-        this.router.navigate(['/']);
+        this.router.navigate(['/search/categories/root']);
       }
-    })
+    });
   }
 
-  getEmailErrors(){
+  getEmailErrors() {
     return this.emailControl.hasError('required') ? "Email is required" :
-        this.emailControl.hasError('email') ? "Email entered is not valid" :
+      this.emailControl.hasError('email') ? "Email entered is not valid" :
         "Email entered could not be read"
   }
 
-  getPassErrors(){
+  getPassErrors() {
     return this.passControl.hasError('required') ? "Password is required" :
-        "Password is incorrect"
+      "Password is incorrect"
   }
 
-  getCIDErrors(){
+  getCIDErrors() {
     return this.CIDControl.hasError('required') ? "Company ID is required" :
-        "Company ID is incorrect"
+      "Company ID is incorrect"
   }
 
-  onSubmit(){
+  onSubmit() {
     this.authService.login(
       this.loginForm.value.email,
       this.loginForm.value.password,
       this.loginForm.value.CID).then(res => {
-        this.router.navigate(['/search/locations/root'])
-      }).catch(err => {
-        this.snack.open('Login Failed: '+err, "OK", {duration: 3000});
-      });
+      this.router.navigate(['/search/locations/root']);
+    }).catch(err => {
+      this.snack.open('Login Failed: ' + err, "OK", {duration: 3000});
+    });
   }
 
   /**
    * Logic for forgetting a password, launches modal
    */
-  forgotPassword(){
-    this.diag.open(ResetPassDialogComponent,{
-      width: '60%'
-    }
+  forgotPassword() {
+    this.diag.open(ResetPassDialogComponent, {
+        width: '60%'
+      }
     ).afterClosed().subscribe(
       val => {
         this.authService.sendPasswordResetEmail(val).then(
@@ -86,8 +91,8 @@ export class LoginComponent implements OnInit {
   /**
    * Opens link to a new workspace
    */
-  newWorkspace(){
-    window.open("https://google.com","_blank");
+  newWorkspace() {
+    window.open("https://google.com", "_blank");
   }
 
 }
