139c7b53e97a8beaad78c157ce5c47a6174f4ca7
Peter <peter.lowrance@gmail.com>
2020-03-08 23:25:55 -0400

    modify hierarchy page adding working

--- src/app/components/modify-hierarchy/modify-hierarchy.component.ts
+++ src/app/components/modify-hierarchy/modify-hierarchy.component.ts
@@ -4,14 +4,13 @@ import {NestedTreeControl} from '@angular/cdk/tree';
 import {SearchService} from '../../services/search.service';
 import {MatTreeNestedDataSource} from '@angular/material/tree';
 import {ActivatedRoute} from '@angular/router';
-import {ReportDialogComponent} from '../report-dialog/report-dialog.component';
-import {MatDialog} from "@angular/material/dialog";
-import {EditHierarchyDialogComponent} from "../edit-hierarchy-dialog/edit-hierarchy-dialog.component";
-import {BehaviorSubject, of, zip} from "rxjs";
-import {map} from "rxjs/operators";
+import {MatDialog} from '@angular/material/dialog';
+import {EditHierarchyDialogComponent} from '../edit-hierarchy-dialog/edit-hierarchy-dialog.component';
+import {BehaviorSubject, of, zip} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 interface TreeHierarchyItem extends HierarchyItem {
-  realChildren?: TreeHierarchyItem[];
+  addedChildren?: TreeHierarchyItem[];
 }
 
 @Component({
@@ -22,69 +21,34 @@ interface TreeHierarchyItem extends HierarchyItem {
 export class ModifyHierarchyComponent implements OnInit {
   isCategory: boolean;
   treeControl = new NestedTreeControl<TreeHierarchyItem>(node => {
-    // If you have real children, return them. Otherwise fetch children from database
-    this.searchService.getDescendantsOfRoot(node.ID, this.isCategory).subscribe(data => node.realChildren = data);
-    return node.realChildren;
+    // Merge the children from the database and that additional children
+    return zip(this.searchService.getDescendantsOfRoot(node.ID, this.isCategory), of(node.addedChildren))
+      .pipe(map(x => x[0].concat(x[1])));
   });
   dataSource = new MatTreeNestedDataSource<TreeHierarchyItem>();
   dataChange = new BehaviorSubject<TreeHierarchyItem[]>([]);
 
-  // toTree = (h: HierarchyItem) => ({name: h.name, imageUrl: h.imageUrl, children: [], ID: h.ID});
-
-  hasNoContent = (_: number, nodeData: TreeHierarchyItem) => nodeData.name === '';
-
-  hasChild = (_: number, node: TreeHierarchyItem) => (!!node.children && node.children.length > 0) || (!!node.realChildren && node.realChildren.length > 0);
+  hasChild = (_: number, node: TreeHierarchyItem) => (!!node.children && node.children.length > 0) ||
+    (!!node.addedChildren && node.addedChildren.length > 0)
 
   constructor(private searchService: SearchService, private route: ActivatedRoute, public dialog: MatDialog) {
   }
 
   ngOnInit() {
     this.isCategory = this.route.snapshot.paramMap.get('selectedHierarchy') === 'categories';
-    // this.treeControl = new NestedTreeControl<HierarchyItem>(node => this.searchService.getDescendantsOfRoot(node.ID, isCategory));
-    this.searchService.getDescendantsOfRoot('root', this.isCategory).subscribe(items => {
-      // this.dataSource.data = items;
-      this.dataChange.subscribe(x => {
-        this.dataSource.data = null;
-        this.dataSource.data = x;
-      });
-      this.dataChange.next(items);
+    this.dataChange.subscribe(changedData => {
+      this.dataSource.data = null;
+      this.dataSource.data = changedData;
+    });
+    this.searchService.getDescendantsOfRoot('root', this.isCategory).subscribe(descOfRoot => {
+      this.dataChange.next(descOfRoot);
     });
   }
 
-  refreshTree() {
-    const data = this.dataSource.data;
-    this.dataSource.data = null;
-    this.dataSource.data = data;
-  }
-
-  editItem(node: TreeHierarchyItem) {
-    console.log(node.realChildren);
-    console.log(node.children);
-    this.searchService.getDescendantsOfRoot(node.ID, this.isCategory).subscribe(d => console.log(d));
-    const newItem: TreeHierarchyItem = {
-      name: 'test',
-      ID: '1234',
-      children: [],
-      realChildren: [],
-      items: []
-    };
-    if (!node.realChildren) {
-      node.realChildren = [newItem];
-    } else {
-      node.realChildren.push(newItem);
-    }
-    if (!this.treeControl.isExpanded(node)) {
-      this.treeControl.expand(node);
-    }
-    this.dataChange.next(this.dataChange.value);
-    // this.treeControl.dataNodes.find(element => element.ID === node.ID).children.push('root');
-    // this.treeControl.collapseAll();
+  openEditModal(node: TreeHierarchyItem) {
     const dialogRef = this.dialog.open(EditHierarchyDialogComponent, {
       width: '75%',
-      data: {
-        name: node.name,
-        imageUrl: node.imageUrl
-      }
+      data: node
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -103,5 +67,25 @@ export class ModifyHierarchyComponent implements OnInit {
         this.report.reportDate = new Date().toDateString();
       }*/
     });
+  }
+
+  /**
+   * Add a new item to the hierarchy
+   * @param parent the parent of the item to be added
+   * @param newNode the new node to be added
+   */
+  addItem(parent: TreeHierarchyItem, newNode: TreeHierarchyItem) {
+    // Add new node to parent
+    if (!parent.addedChildren) {
+      parent.addedChildren = [newNode];
+    } else {
+      parent.addedChildren.push(newNode);
+    }
+    // Expand parent
+    if (!this.treeControl.isExpanded(parent)) {
+      this.treeControl.expand(parent);
+    }
+    // Set data
+    this.dataChange.next(this.dataChange.value);
   }
 }
