ada83f06d6add787930d800e46d5934012aee0f6
Zowja <mszowja@gmail.com>
2020-07-20 13:10:13 -0400

    Included Item screen's attributes in sorting
    1.2.5 (updated)

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -362,6 +362,20 @@ export class ItemComponent implements OnInit, OnDestroy {
         })
       }
     }
+
+    cards.sort(function(a, b) {
+      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+    
+      // names must be equal
+      return 0;
+    })
     return cards;
   }
 
