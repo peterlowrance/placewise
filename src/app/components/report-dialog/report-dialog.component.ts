78f85a9f6db3cff90c0247f80294a28f6dd7a30d
Zowja <mszowja@gmail.com>
2021-03-05 16:34:11 -0500

    Complete report dialog

--- src/app/components/report-dialog/report-dialog.component.ts
+++ src/app/components/report-dialog/report-dialog.component.ts
@@ -29,11 +29,16 @@ export class ReportDialogComponent implements OnInit {
   ) { }
 
   step: string;
-  loadingUsers = false;
+  loading = {
+    custom: false,
+    low: false,
+    empty: false
+  }
   admins: WorkspaceUser[];
   selectedAdmins: WorkspaceUser[];
   description: string = '';
   locationID = 'none';
+  isAutoReport = false;
 
   ngOnInit() {
     this.step = 'start';
@@ -41,17 +46,17 @@ export class ReportDialogComponent implements OnInit {
 
   onNextClick() {
     if(this.step === 'start'){
-      this.loadingUsers = true;
+      this.loading.custom = true;
       this.adminService.getWorkspaceUsers().subscribe(users => {
         if(users && users.length === this.authService.usersInWorkspace){
-          this.loadingUsers = false;
+          this.loading.custom = false;
 
           // Load admins for selection
           this.admins = users.filter(element => { return element.role === "Admin" });
           // Load selected people to report to
           this.selectedAdmins = this.admins.filter(element => { return this.authService.workspace.defaultUsersForReports.indexOf(element.id) > -1 });
           
-          // If there's no location, keep the locationID to 'none' and skip to who
+          // If there's no location, keep the locationID to 'none' and skip a step
           if(!this.data.locations || this.data.locations.length < 1){
             this.step = 'who';
           }
@@ -74,22 +79,66 @@ export class ReportDialogComponent implements OnInit {
 
   setLocation(locationID: string){
     this.locationID = locationID;
-    this.step = 'who';
+
+    if(this.isAutoReport){
+      this.sendReport();
+    }
+    else {
+      this.step = 'who';
+    }
   }
 
   onSendClick(){
     if(this.description == '') this.description = PLACEHOLDER;
     this.sendReport();
-    this.dialogRef.close({wasValid: true});
   }
 
   onCancelClick(){
     this.dialogRef.close({wasValid: false});
   }
 
+  setupAutoReport(message: string){
+
+    if(message === "low"){
+      this.description = "Item is low."
+      this.loading.low = true;
+    }
+    else {
+      this.description = "Item is empty!"
+      this.loading.empty = true;
+    }
+
+    this.adminService.getWorkspaceUsers().subscribe(users => {
+      if(users && users.length === this.authService.usersInWorkspace){
+         
+        // Load admins for selection
+        this.admins = users.filter(element => { return element.role === "Admin" });
+        // Load selected people to report to
+        this.selectedAdmins = this.admins.filter(element => { return this.authService.workspace.defaultUsersForReports.indexOf(element.id) > -1 });
+
+        // If there's no location or only one, set that up quick and report immediately
+        if(!this.data.locations || this.data.locations.length < 2){
+          if(this.data.locations.length === 1){
+            this.locationID = this.data.locations[0].ID;
+          }
+          this.sendReport();
+        }
+        
+        // Otherwise specify which location
+        else {
+          this.isAutoReport = true;
+          this.step = 'where';
+        }
+      }
+    })
+  }
+
   sendReport(){
+    this.dialogRef.close({wasValid: true});
+    this.snack.open("Sending Report...", '', {duration: 2000, panelClass: ['mat-toolbar']});
+
     this.adminService.placeReport(this.data.item.ID, this.description, this.selectedAdmins.map(user => user.id), this.locationID).then(
-      () => this.snack.open("Report Sent", "OK", {duration: 3000, panelClass: ['mat-toolbar']}),
+      () => this.snack.open("Report Sent!", "OK", {duration: 4000, panelClass: ['mat-toolbar']}),
       (err) => {
         this.snack.open("Report Failed, " + err.status, "OK", {duration: 10000, panelClass: ['mat-toolbar']})
         console.log(JSON.stringify(err));
